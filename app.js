39cb6780352a406a555952966b1d529f143c3d7a
abubakirmajidov <abubakirmajidov@gmail.com>
2024-02-16 22:26:37 +0500

    abubakir

---
+++ app.js
@@ -0,0 +1,33 @@
+/* let yosh = prompt("yosh");
+
+if (yosh < 18) {
+  console.log(
+    `Sizga armiaga borolmaysiz. Chunke siz kichkinasiz. ${
+      18 - yosh
+    } yosh yetmaydi`
+  );
+} else if (yosh >= 18 && yosh < 27) {
+  console.log("Olg'a, vattani himoya qil !");
+} else {
+  console.log(
+    `Siz borish yoshidan katta ekansiz :(. ${yosh - 27} yosh o'tib ketgan`
+  );
+} */
+
+/* let nomber1 = Number(prompt("birinchi raqamni kiriting "));
+let nomber2 = Number(prompt("ikkinci raqamni kiriting "));
+let amal = prompt("amalni kiriting ");
+
+if (amal == "+") {
+  alert(nomber1 + nomber2);
+} else if (amal == "-") {
+  alert(nomber1 - nomber2);
+} else if (amal == "*") {
+  alert(nomber1 * nomber2);
+} else if (amal == "/") {
+  alert(nomber1 / nomber2);
+} else if (amal == "**") {
+  alert(nomber1 ** nomber2);
+} else {
+  alert("buday amal mavjut emas");
+} */
